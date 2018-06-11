d3df070e9f628ee0a4989049e7ca5f0bbf0d404c
Mirza Pasic <mirza.pasic@bosnadev.com>
2018-06-11 23:18:34 +0200

    Added FormBuilder.

---
+++ src/components/FormBuilder.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Formio from 'formiojs/Formio'
+import FormBuilder from 'formiojs/FormBuilder'
+import AllComponents from 'formiojs/components'
+import Components from 'formiojs/components/Components'
+import { Component, Prop, Watch } from 'vue-property-decorator'
+
+// Load all Formio components
+Components.setComponents(AllComponents)
+
+@Component
+export default class VueFormBuilder extends Vue {
+  // FormBuilder instance
+  builder
+  builderReady
+
+  @Prop({ type: Object })
+  form
+
+  @Prop({ default: {} })
+  options
+
+  formBuilderInit () {
+    if (!this.form) {
+      return Promise.reject(new Error('You must set form attribute.'))
+    }
+
+    this.builder = new FormBuilder(this.$refs.formio, this.form, this.options)
+    this.builderReady = this.builder.setDisplay(this.form.display)
+
+    return this.builderReady.then(() => {
+      this.builder.instance.events.onAny((...args) => {
+        const eventParts = args[0].split('.');
+
+        // Only handle formio events.
+        if (eventParts[0] !== 'formio' || eventParts.length !== 2) {
+          return;
+        }
+
+        // Emit a change event if the schema changes.
+        if (['saveComponent', 'updateComponent', 'deleteComponent'].includes(eventParts[1])) {
+          args[0] = 'change';
+          this.$emit.apply(this, args);
+        }
+      })
+    })
+  }
+
+  mounted () {
+    this.formBuilderInit()
+      .catch(e => {
+        // eslint-disable-next-line no-console
+        console.error(e)
+      })
+  }
+
+  render (createElement) {
+    return createElement('div', { ref: 'formio' })
+  }
+}
